ef38190102a3471fcdbf55a3b5c81aba21d77d72
Elnur <mikailov.03@list.ru>
2025-07-14 18:19:24 +0300

    Fix prime game structure and output format

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,26 +1,4 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import { greetUser } from '../src/cli.js';
-import isPrime from '../src/games/prime.js';
+import runPrimeGame from '../src/games/prime.js'
 
-const playPrimeGame = () => {
-  const name = greetUser();
-  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-
-  for (let i = 0; i < 3; i++) {
-    const num = Math.floor(Math.random() * 100) + 1;
-    const correctAnswer = isPrime(num) ? 'yes' : 'no';
-    const userAnswer = readlineSync.question(`Question: ${num}\nYour answer: `);
-
-    if (userAnswer !== correctAnswer) {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${name}!`);
-      return;
-    }
-    console.log('Correct!');
-  }
-
-  console.log(`Congratulations, ${name}!`);
-};
-
-playPrimeGame();
+runPrimeGame()
--- src/games/prime.js
+++ src/games/prime.js
@@ -1,7 +1,7 @@
-import runGame from '../index.js'
-import { getRandomNumber } from '../utils.js'
+import { getRandomNumber } from '../utils.js'  
+import runGame from '../index.js'             
 
-const isPrime = num => {
+const isPrime = (num) => {
   if (num < 2) return false
   for (let i = 2; i <= Math.sqrt(num); i += 1) {
     if (num % i === 0) return false
@@ -11,11 +11,11 @@ const isPrime = num => {
 
 const generateRound = () => {
   const number = getRandomNumber(1, 100)
-  const question = `Question: ${number}` 
+  const question = `Question: ${number}`
   const answer = isPrime(number) ? 'yes' : 'no'
   return [question, answer]
 }
 
-export default () => {
-  runGame('Answer "yes" if given number is prime. Otherwise answer "no".', generateRound)
-}
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+export default () => runGame(description, generateRound)
