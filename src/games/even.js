32126336415ca44edea6b585c83fa75a0f5efd1d
Elnur <mikailov.03@list.ru>
2025-07-14 17:52:29 +0300

    Remove all semicolons and fix arrow functions

--- src/games/even.js
+++ src/games/even.js
@@ -1,2 +1,15 @@
-export const isEven = (number) => number % 2 === 0;
+import runGame from '../index.js'
+import { getRandomNumber } from '../utils.js'
 
+const isEven = num => num % 2 === 0
+
+const generateRound = () => {
+  const number = getRandomNumber(1, 100)
+  const question = `Question: ${number}`
+  const answer = isEven(number) ? 'yes' : 'no'
+  return [question, answer]
+}
+
+export default () => {
+  runGame('Answer "yes" if the number is even, otherwise answer "no".', generateRound)
+}
--- src/games/prime.js
+++ src/games/prime.js
@@ -1,9 +1,21 @@
-const isPrime = (num) => {
-  if (num < 2) return false;
-  for (let i = 2, max = Math.sqrt(num); i <= max; i++) {
-    if (num % i === 0) return false;
-  }
-  return true;
-};
+import runGame from '../index.js'
+import { getRandomNumber } from '../utils.js'
 
-export default isPrime;
+const isPrime = num => {
+  if (num < 2) return false
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) return false
+  }
+  return true
+}
+
+const generateRound = () => {
+  const number = getRandomNumber(1, 100)
+  const question = `Question: ${number}`
+  const answer = isPrime(number) ? 'yes' : 'no'
+  return [question, answer]
+}
+
+export default () => {
+  runGame('Answer "yes" if given number is prime. Otherwise answer "no".', generateRound)
+}
