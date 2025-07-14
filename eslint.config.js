f7227473e4917e5a9b9e16ab7dc4eb6cc9bd0fff
Elnur <mikailov.03@list.ru>
2025-07-14 17:32:53 +0300

    Fix all ESLint errors

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,12 @@
-import js from '@eslint/js';
-import globals from 'globals';
+import js from '@eslint/js'
+import globals from 'globals'
 
 export default [
   js.configs.recommended,
   {
+    plugins: {
+      '@stylistic': require('@stylistic/eslint-plugin')
+    },
     languageOptions: {
       globals: {
         ...globals.browser,
@@ -11,7 +14,13 @@ export default [
       }
     },
     rules: {
-      'no-console': 'off'
+      'no-console': 'off',
+      '@stylistic/semi': ['error', 'never'],
+      '@stylistic/eol-last': ['error', 'always'],
+      '@stylistic/comma-dangle': ['error', 'never'],
+      '@stylistic/arrow-parens': ['error', 'as-needed'],
+      '@stylistic/no-multi-spaces': 'error',
+      '@stylistic/no-multiple-empty-lines': ['error', { max: 1 }]
     }
   }
-];
+]
