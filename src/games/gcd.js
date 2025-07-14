376921096e3b6fb2f0f531c74c2abecde538c87b
Elnur <mikailov.03@list.ru>
2025-07-14 17:08:26 +0300

    Fix question format in brain-gcd

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -9,7 +9,7 @@ const findGCD = (a, b) => {
 const generateRound = () => {
   const num1 = getRandomNumber(1, 100);
   const num2 = getRandomNumber(1, 100);
-  const question = `${num1} ${num2}`;
+  const question = `Question: ${num1} ${num2}`;
   const correctAnswer = String(findGCD(num1, num2));
   return [question, correctAnswer];
 };
