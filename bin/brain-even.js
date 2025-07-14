5d080766b35c42a608094a97f6588feba8169848
Elnur <mikailov.03@list.ru>
2025-07-14 17:57:47 +0300

    Fix even game exports

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,26 +1,26 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import { greetUser } from '../src/cli.js';
-import { isEven } from '../src/games/even.js';
+import readlineSync from 'readline-sync'
+import { greetUser } from '../src/cli.js'
+import { isEven } from '../src/games/even.js'
 
 const playEvenGame = () => {
-  const name = greetUser();
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  const name = greetUser()
+  console.log('Answer "yes" if the number is even, otherwise answer "no".')
 
   for (let i = 0; i < 3; i++) {
-    const number = Math.floor(Math.random() * 100);
-    const correctAnswer = isEven(number) ? 'yes' : 'no';
-    const userAnswer = readlineSync.question(`Question: ${number}\nYour answer: `);
+    const number = Math.floor(Math.random() * 100)
+    const correctAnswer = isEven(number) ? 'yes' : 'no'
+    const userAnswer = readlineSync.question(`Question: ${number}\nYour answer: `)
 
     if (userAnswer !== correctAnswer) {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${name}!`);
-      return;
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`)
+      console.log(`Let's try again, ${name}!`)
+      return
     }
-    console.log('Correct!');
+    console.log('Correct!')
   }
 
-  console.log(`Congratulations, ${name}!`);
-};
+  console.log(`Congratulations, ${name}!`)
+}
 
-playEvenGame();
+playEvenGame()
--- src/games/even.js
+++ src/games/even.js
@@ -1,7 +1,7 @@
 import runGame from '../index.js'
 import { getRandomNumber } from '../utils.js'
 
-const isEven = num => num % 2 === 0
+export const isEven = num => num % 2 === 0  // Добавлен экспорт
 
 const generateRound = () => {
   const number = getRandomNumber(1, 100)
