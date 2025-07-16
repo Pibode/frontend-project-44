395c29c893a9f4385df46b80213fad68df877c9c
Elnur <mikailov.03@list.ru>
2025-07-16 19:23:49 +0300

    Fix brain-even.js structure

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,26 +1,4 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync'
-import { greetUser } from '../src/cli.js'
-import { isEven } from '../src/games/even.js'
+import startEvenGame from '../src/games/even.js'
 
-const playEvenGame = () => {
-  const name = greetUser()
-  console.log('Answer "yes" if the number is even, otherwise answer "no".')
-
-  for (let i = 0; i < 3; i++) {
-    const number = Math.floor(Math.random() * 100)
-    const correctAnswer = isEven(number) ? 'yes' : 'no'
-    const userAnswer = readlineSync.question(`Question: ${number}\nYour answer: `)
-
-    if (userAnswer !== correctAnswer) {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`)
-      console.log(`Let's try again, ${name}!`)
-      return
-    }
-    console.log('Correct!')
-  }
-
-  console.log(`Congratulations, ${name}!`)
-}
-
-playEvenGame()
+startEvenGame()
